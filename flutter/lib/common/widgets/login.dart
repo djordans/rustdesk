d77215b37218ecd646ab0be842410bac62d1e06c
djordans <jordan.grundheber@gmail.com>
2023-03-23 15:27:54 +0100

    Update login.dart

--- flutter/lib/common/widgets/login.dart
+++ flutter/lib/common/widgets/login.dart
@@ -478,6 +478,7 @@ Future<bool?> loginDialog() async {
             if (resp.access_token != null) {
               await bind.mainSetLocalOption(
                   key: 'access_token', value: resp.access_token!);
+              gFFI.userModel.userName.value = username;
               close(true);
               return;
             }
