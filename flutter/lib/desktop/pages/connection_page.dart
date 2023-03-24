b5eb4106a8c82c7bbd99a1084aa9d6fbd0945fae
djordans <jordan.grundheber@gmail.com>
2023-03-24 10:23:00 +0100

    Update connection_page.dart

--- flutter/lib/desktop/pages/connection_page.dart
+++ flutter/lib/desktop/pages/connection_page.dart
@@ -151,7 +151,8 @@ class _ConnectionPageState extends State<ConnectionPage>
   /// Connects to the selected peer.
   void onConnect({bool isFileTransfer = false}) {
     var id = _idController.id;
-    connect(context, id, isFileTransfer: isFileTransfer);
+    var password = gFFI.abModel.getPeerPassword(id);
+    connect(context, id, isFileTransfer: isFileTransfer,password: password);
   }
 
   /// UI for the remote ID TextField.
