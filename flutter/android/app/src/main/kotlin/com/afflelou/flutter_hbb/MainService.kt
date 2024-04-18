6a94554956548ae42522e9f87be7b59f430bc058
djordans <jordan.grundheber@gmail.com>
2024-04-18 13:52:10 +0200

    fix android 14 #4

--- flutter/android/app/src/main/kotlin/com/afflelou/flutter_hbb/MainService.kt
+++ flutter/android/app/src/main/kotlin/com/afflelou/flutter_hbb/MainService.kt
@@ -621,7 +621,7 @@ class MainService : Service() {
         }
         val pendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                val options = ActivityOptions.makeBasic().setPendingIntentBackgroundActivityStartMode(ActivityOptions.MODE_BACKGROUND_ACTIVITY_START_ALLOWED) 
+                //val options = ActivityOptions.makeBasic().setPendingIntentBackgroundActivityStartMode(ActivityOptions.MODE_BACKGROUND_ACTIVITY_START_ALLOWED) 
                 PendingIntent.getActivity(this, 0, intent, FLAG_UPDATE_CURRENT or FLAG_IMMUTABLE)
             }else{
                 PendingIntent.getActivity(this, 0, intent, FLAG_UPDATE_CURRENT or FLAG_IMMUTABLE)
